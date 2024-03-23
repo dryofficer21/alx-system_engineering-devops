c11c3e66fc657d4787673641e890f8875d5dbb9a
Dryofficer21 <ilyassfakhtaoui5@gmail.com>
2024-03-22 17:07:25 -0700

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
 # Install an especific version of flask (2.1.0)
-package {'flask':
+package { 'pip3':
+  ensure   => installed,
+  provider => 'apt',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['pip3'],
 }
