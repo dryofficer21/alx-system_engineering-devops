4ab5a980495eeed0ea50d1e29d89e2d0b24911ff
Dryofficer21 <ilyassfakhtaoui5@gmail.com>
2024-06-04 16:29:15 +0000

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
 """
-0x16. API advanced
+Contains the number_of_subscribers function
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """A function that returns the number of subscribers
-    to an account if existed."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(
-        url,
-        headers={"User-Agent": "custom"},
-    )
-    if response.status_code == 200:
-        return response.json()["data"]["subscribers"]
-    else:
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
         return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
